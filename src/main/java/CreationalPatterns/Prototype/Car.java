eba9a811646da09e8b17d4bfc501222a4c83a314
Egor <egorborisovzypo@gmail.com>
2022-01-28 17:00:08 +0600

    Initial commit

---
+++ src/main/java/CreationalPatterns/Prototype/Car.java
@@ -0,0 +1,52 @@
+package CreationalPatterns.Prototype;
+
+public class Car implements Prototype {
+    private int id;
+    private String carName;
+    private String carDescription;
+
+    public Car(int id, String carName, String carDescription) {
+        this.id = id;
+        this.carName = carName;
+        this.carDescription = carDescription;
+    }
+
+    @Override
+    public Object clone() {
+        Car copy = new Car(id, carName, carDescription);
+        return copy;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return carName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.carName = projectName;
+    }
+
+    public String getCarDescription() {
+        return carDescription;
+    }
+
+    public void setCarDescription(String carDescription) {
+        this.carDescription = carDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", carName='" + carName + '\'' +
+                ", carDescription='" + carDescription + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/CreationalPatterns/Prototype/Main.java
@@ -0,0 +1,12 @@
+package CreationalPatterns.Prototype;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car(1,"Volvo", "120 л.с, 88Квт\n");
+    System.out.println(car1);
+
+   Car car1Clone = (Car) car1.clone();
+    System.out.println("\n=========================");
+    System.out.println(car1Clone);
+    }
+}
