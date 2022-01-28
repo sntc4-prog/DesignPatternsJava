003143df9f1c6fc427f7b9c88498c0384a35dfa9
Egor <egorborisovzypo@gmail.com>
2022-01-29 00:49:21 +0600

    Added decorator

---
+++ src/main/java/structuralpatterns/decorator/Developer.java
@@ -0,0 +1,5 @@
+package structuralpatterns.decorator;
+
+public interface Developer {
+    String makeJob();
+}
---
+++ src/main/java/structuralpatterns/decorator/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package structuralpatterns.decorator;
+
+public class JavaDeveloper implements Developer{
+    @Override
+    public String makeJob() {
+        return "Write Java code";
+    }
+}
