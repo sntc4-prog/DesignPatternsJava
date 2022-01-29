7e78b6f327eb24f908e1d151926b03367b9c5df8
Egor <egorborisovzypo@gmail.com>
2022-01-29 10:08:31 +0600

    Added facade

---
+++ src/main/java/structuralpatterns/facade/BugTracker.java
@@ -0,0 +1,18 @@
+package structuralpatterns.facade;
+
+public class BugTracker {
+    private boolean activeSprint;
+
+    public boolean isActiveSprint() {
+        return activeSprint;
+    }
+
+    public void startSprint(){
+        System.out.println("Sprint is active");
+        activeSprint = true;
+    }
+    public void finishSprint(){
+        System.out.println("Sprint is not active");
+        activeSprint = false;
+    }
+}
---
+++ src/main/java/structuralpatterns/facade/Developer.java
@@ -0,0 +1,12 @@
+package structuralpatterns.facade;
+
+public class Developer {
+    public void doJobBeforeDeadline(BugTracker bugTracker){
+        if (bugTracker.isActiveSprint()){
+            System.out.println("Developer is solving problem...");
+        }else{
+            System.out.println("Developer is reading Habrahabr...");
+        }
+
+    }
+}
